778a7feacb93de333e9251c04df086965e6eb3fd
plevy <plevy@vhio.net>
2025-12-05 12:10:30 +0100

    update to OA 2.2.0

--- RUN.oncoanalyser_hg38_WES.sh
+++ RUN.oncoanalyser_hg38_WES.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name=OA    # Job name
+#SBATCH --mem=8gb # Job memory request
+#SBATCH -c 2 # max nr threads
+#SBATCH --time=5-00:00:00           # Time limit hrs:min:sec
+#SBATCH --output=R-%x.%j.out   # Standard output and error log
+#SBATCH --error=R-%x.%j.err
+#SBATCH --partition=highmem
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=plevy@vhio.net
 
 # DO NOT CHANGE THESE LINES
-INPUT=${1}
-OUTPUT=${2}
+INPUT=${1:-"input.csv"}
+OUTPUT=${2:-"output"}
 OPT_ARGS=${3:-""} # optional arguments to pass to nextflow
 
 # Create cache and tmp directories if they don't exist
@@ -13,15 +22,15 @@ mkdir -p tmp
 nextflow run nf-core/oncoanalyser \
   -profile singularity \
   -revision 2.2.0 \
-  --mode wgts \
-  -params-file /mnt/bioinfnas/immuno/plevy/proj/hmftools/hmf38.yaml \
+  --mode targeted \
+  --panel wes \
   --genome GRCh38_hmf \
-  --ref_data_hmf_data_path /mnt/bioinfnas/immuno/Jonatan/References/hmftools_ref/reference_data/2.2.0/hmf_pipeline_resources.38_v2.2.0--3 \
+  --force_panel \
   --input ${INPUT} \
   --outdir ${OUTPUT} \
+  --ref_data_hmf_data_path /mnt/bioinfnas/immuno/Jonatan/References/hmftools_ref/reference_data/2.2.0/hmf_pipeline_resources.38_v2.2.0--3 \
   -c /mnt/bioinfnas/immuno/plevy/proj/hmftools/hmf.local.config \
-  -resume \
-  ${OPT_ARGS} 
+  -resume
 
   ## INFOS #################################################################################################################################################################################################################################################################
   # Make sure the following command was previously ran to consider Ensembl NMD-labeled transcripts as protein coding
@@ -32,5 +41,4 @@ nextflow run nf-core/oncoanalyser \
   ###########################################################################################################################################################################################################################################################################
 
   # Useful Options
-  # --processes_manual \
-  # --processes_include alignment,redux,cobalt,amber,sage
+  # --processes_manual alignment,redux,cobalt,amber,sage
